908edca84741e8fa7ff908b8098440557d78943c
Shasawat Pal <shashwatpal2001@gmail.com>
2024-12-22 01:04:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Shasawat Pal",
+  title: "Web Developer & Content Creator",
+  email: "shashwatpal2001@gmail.com",
+  gitHub: "https://github.com/shash319",
+  linkedIn: "https://www.linkedin.com/in/shasawat-pal-9506781a9/",
+  twitter: "https://x.com/p_shasawat_bro",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,32 @@
+/**
+ * Header component
+ *
+ * Top navigation bar for your site. Set to remain visible as the
+ * user scrolls so that they can constantly reach any part of your page.
+ */
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        display: "flex",
+        justifyContent: "center",
+        gap: "5rem",
+        background: "lightblue",
+        padding: "1rem",
+        top: 0,
+        width: "100%",
+        zIndex: 10,
+      }}
+    >
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#footer">Contact</a>
+    </div>
+  );
+};
+
+export default Header;
